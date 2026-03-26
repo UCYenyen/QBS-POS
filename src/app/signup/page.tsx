a083379f8876957d915a08d4a3c2b812d804c3a9
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-03-26 17:05:09 +0700

    cool login register background

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,27 @@
 import { SignupForm } from "@/components/signup-form"
+import LightRays from "@/components/LightRays"
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
+    <div className="relative flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="absolute inset-0 w-full h-full pointer-events-none">
+        <LightRays
+          raysOrigin="top-center"
+          raysColor="#846feb"
+          raysSpeed={0.25}
+          lightSpread={1}
+          rayLength={10}
+          followMouse={true}
+          mouseInfluence={0.05}
+          noiseAmount={0.5}
+          distortion={0.1}
+          className="custom-rays"
+          pulsating={true}
+          fadeDistance={1}
+          saturation={1}
+        />
+      </div>
+      <div className="relative z-10 w-full max-w-sm">
         <SignupForm />
       </div>
     </div>
